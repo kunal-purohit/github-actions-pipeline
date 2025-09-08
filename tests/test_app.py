4bae5d093a1da88576f238b0719ae4e504a80e0b
Kunal Purohit <kunal.purohit@datagrokr.co>
2025-09-08 15:21:09 +0530

    Final Commit

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,5 @@
-# import sys, os
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from app import add, subtract, multiply, divide
 import pytest
